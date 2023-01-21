0afef1e777fdbed1a00d343a47378881bacc4714
Aparecida <cidinha.dev@gmail.com>
2023-01-21 15:51:48 -0300

    adicionando conteudo

---
+++ src/IncrementoDecremento.java
@@ -0,0 +1,24 @@
+
+public class IncrementoDecremento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Integer numero = 10;
+		System.out.println("Número: "+numero);
+		
+		
+		// incremento antes é atribuido a varial e o incremento depos não (numero ++)
+		Integer numero2 = ++ numero ;
+		
+		System.out.println("Número: "+numero2);
+		
+
+
+
+		
+	}
+
+}
